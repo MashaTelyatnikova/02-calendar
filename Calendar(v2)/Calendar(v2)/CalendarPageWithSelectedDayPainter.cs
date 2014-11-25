66a73134a51db9d5bcfa44b0a0c388f004a03f3f
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-26 01:25:58 +0500

    Добавлены функции для отрисовки.

--- Calendar(v2)/Calendar(v2)/CalendarPageWithSelectedDayPainter.cs
+++ Calendar(v2)/Calendar(v2)/CalendarPageWithSelectedDayPainter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -46,6 +47,83 @@ namespace Calendar_v2_
         {
             this.calendarPage = calendarPage;
             this.selectedDay = selectedDay;
+        }
+
+        public Image Paint()
+        {
+            var calendarImage = new Bitmap(CalendarPageWidth, CalendarPageHeight);
+            var graphics = Graphics.FromImage(calendarImage);
+            graphics.Clear(calendarBackColor);
+
+            DrawAnimal(graphics);
+            DrawHeader(graphics);
+            DrawWeekDays(graphics);
+            DrawCalendarPageLines(graphics);
+
+            return calendarImage;
+        }
+
+        private void DrawAnimal(Graphics graphics)
+        {
+            var animal = EasternHoroscope.GetAnimalOfYear(calendarPage.Year);
+            graphics.DrawImage(new Bitmap(imageOfAnimal[animal]), 0, 0);
+        }
+
+        private void DrawHeader(Graphics graphics)
+        {
+            graphics.DrawRectangle(boundaryPen, StartX, StartY - CellHeight, 7 * CellWidth, CellHeight);
+
+            graphics.DrawString(Months.GetMonthName(calendarPage.Month) + " " + calendarPage.Year,
+                            textFont,
+                            textBrush,
+                            StartX,
+                            StartY - CellHeight
+                           );
+        }
+
+        private void DrawWeekDays(Graphics graphics)
+        {
+            var weekDays = new[] { "Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun" };
+            for (var i = 0; i < weekDays.Count(); ++i)
+            {
+                graphics.DrawRectangle(boundaryPen, StartX + i * CellWidth, StartY, CellWidth, CellHeight);
+                graphics.DrawString(weekDays[i], textFont, textBrush, StartX + i * CellWidth, StartY);
+            }
+        }
+
+        private void DrawCalendarPageLines(Graphics graphics)
+        {
+            var lines = calendarPage.GetCalendarPageLines().ToList();
+            for (var i = 0; i < lines.Count; ++i)
+                DrawCalendarPageLine(lines[i], i + 1, graphics);
+
+        }
+
+        private void DrawCalendarPageLine(CalendarPageLine calendarPageLine, int number, Graphics graphics)
+        {
+            var line = calendarPageLine.GetDays();
+            foreach (var day in line)
+            {
+                var offset = (int)day.DayOfWeek == 0 ? 6 : (int)day.DayOfWeek - 1;
+                if (day.Number == selectedDay)
+                    graphics.FillEllipse(new SolidBrush(selectionColor),
+                                            StartX + offset * CellWidth,
+                                            StartY + number * CellHeight,
+                                            SelectionWidth,
+                                            SelectionHeight
+                                          );
+
+                graphics.DrawString(day.Number.ToString(CultureInfo.InvariantCulture),
+                                    textFont,
+                                    new SolidBrush(GetColorDayOfWeek(day.DayOfWeek)),
+                                    StartX + offset * CellWidth,
+                                    StartY + number * CellHeight);
+            }
+        }
+
+        private Color GetColorDayOfWeek(DayOfWeek dayOfWeek)
+        {
+            return dayOfWeek == DayOfWeek.Saturday || dayOfWeek == DayOfWeek.Sunday ? holidayColor : weekdayColor;
         }
     }
 }
