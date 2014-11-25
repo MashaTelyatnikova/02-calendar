9578fd909e79af5c3b7b3a40ded6646f83703fb3
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-26 01:17:40 +0500

    Добавлен класс и перечисление для определения животного по году (восточный гороскоп).

---
+++ Calendar(v2)/Calendar(v2)/EasternHoroscope.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar_v2_
+{
+    public static class EasternHoroscope
+    {
+        private const int StartYear = 1948;
+        public static EasternHoroscopeAnimals GetAnimalOfYear(int year)
+        {
+            var result = 0;
+            while (year != StartYear)
+            {
+                if (year <= StartYear)
+                {
+                    result = ((((result - 1) % 12) + 12) % 12);
+                    year++;
+                }
+                else
+                {
+                    result = ((result + 1) % 12);
+                    year--;
+                }
+            }
+            return (EasternHoroscopeAnimals)result;
+        }
+    }
+}
---
+++ Calendar(v2)/Calendar(v2)/EasternHoroscopeAnimals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar_v2_
+{
+    public enum EasternHoroscopeAnimals
+    {
+        Rat = 0,
+        Bull = 1,
+        Tiger = 2,
+        Rabbit = 3,
+        Dragon = 4,
+        Snake = 5,
+        Horse = 6,
+        Sheep = 7,
+        Monkey = 8,
+        Cock = 9,
+        Dog = 10,
+        Pig = 11
+    }
+}
