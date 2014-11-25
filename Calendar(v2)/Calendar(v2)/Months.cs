373ae7ee8fb78a4dc0ab4409db24b9afde1035d4
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-26 01:16:06 +0500

    Добавлен класс Months для получения названия месяца по его номеру.

---
+++ Calendar(v2)/Calendar(v2)/Months.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calendar_v2_
+{
+    public static class Months
+    {
+        private static readonly Dictionary<int, string> MonthNameOfNumber = new Dictionary<int, string>()
+        {
+            {1, "January"},
+            {2, "February"},
+            {3, "March"},
+            {4, "April"},
+            {5, "May"},
+            {6, "June"},
+            {7, "July"},
+            {8, "August"},
+            {9, "September"},
+            {10, "October"},
+            {11, "November"},
+            {12, "December"}
+        };
+
+        public static string GetMonthName(int number)
+        {
+            if (MonthNameOfNumber.ContainsKey(number))
+                return MonthNameOfNumber[number];
+
+            throw new ArgumentException("Incorrect number months");
+        }
+    }
+}
