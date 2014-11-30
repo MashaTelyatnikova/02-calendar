0be2c39c4e6dca57f193573643d7b8feeeae883f
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-30 19:40:37 +0500

    Добавлен проект с тестами. Добавлен класс для тестирования Program.

--- Calendar(v3)/Calendar(v3)/Program.cs
+++ Calendar(v3)/Calendar(v3)/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Calendar_v3_
 {
-    class Program
+    public static class Program
     {
         static void Main(string[] args)
         {
---
+++ Calendar(v3)/CalendarTests/ProgramTests.cs
@@ -0,0 +1,25 @@
+﻿using Calendar_v3_;
+using NUnit.Framework;
+
+namespace CalendarTests
+{
+    [TestFixture]
+    public class ProgramTests
+    {
+        [Test]
+        public void IsValidFormateDate_ValidityDate_ReturnsTrue()
+        {
+            Assert.IsTrue(Program.IsValidFormatDate("26/09/1994"));
+        }
+
+        [TestCase("")]
+        [TestCase("26\\09\\1994")]
+        [TestCase("26/09/1994/1999/987")]
+        [TestCase("12/12/1234      ;lbkv")]
+        [TestCase("lalala12/12/1212")]
+        public void IsValidFormateDate_InvalidDate_ReturnsFalse(string input)
+        {
+            Assert.IsFalse(Program.IsValidFormatDate(input));
+        }
+    }
+}
