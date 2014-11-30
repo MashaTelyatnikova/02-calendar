3925eb08b3cca13f50f2cf031545c261825a56b2
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-30 19:37:17 +0500

    Считывание входных аргументов в мэйне.

--- Calendar(v3)/Calendar(v3)/Program.cs
+++ Calendar(v3)/Calendar(v3)/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Calendar_v3_
@@ -10,6 +11,35 @@ namespace Calendar_v3_
     {
         static void Main(string[] args)
         {
+            if (args.Length != 2)
+            {
+                Console.WriteLine(@"Еnter two arguments - the current date and output filename.");
+                return;
+            }
+
+            var date = args[0];
+            var outputFileName = args[1];
+
+            if (!IsValidFormatDate(date))
+            {
+                Console.WriteLine(@"Invalid format date. The correct format is dd/mm/yyyy");
+                return;
+            }
+
+            DateTime currentDate;
+
+            if (!DateTime.TryParse(date, out currentDate))
+            {
+                Console.WriteLine(@"Incorrect date.");
+                return;
+            }
+        }
+
+        public static bool IsValidFormatDate(string date)
+        {
+            var validFormatRegex = new Regex(@"\d{2}/\d{2}/\d{3}");
+
+            return validFormatRegex.IsMatch(date) && date.Length == 10;
         }
     }
 }
