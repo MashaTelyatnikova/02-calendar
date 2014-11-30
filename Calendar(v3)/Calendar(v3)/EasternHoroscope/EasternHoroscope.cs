4df8f4eb8ab62b4a6b2ae75c2df652803a283056
MashaTelyatnikova <mashatelatnikova@gmail.com>
2014-11-30 19:43:07 +0500

    Добавлены классы для работы с восточным гороскопом.

---
+++ Calendar(v3)/Calendar(v3)/EasternHoroscope/EasternHoroscope.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Calendar_v3_.EasternHoroscope.enums;
+
+namespace Calendar_v3_.EasternHoroscope
+{
+    public static class EasternHoroscope
+    {
+        private const int StartYear = 1948;
+        public static EasternHoroscopeAnimals GetAnimalOfYear(int year)
+        {
+            var difference = Math.Abs(StartYear - year) % 12;
+            if (year <= StartYear)
+                return (EasternHoroscopeAnimals)((12 - difference) % 12);
+            return (EasternHoroscopeAnimals)(difference % 12);
+        }
+    }
+}
